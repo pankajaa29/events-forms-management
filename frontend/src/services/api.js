1ac59398aa704c662e96d0852a4116436587be6e
Pankaj Attarde <pankajaa29@gmail.com>
2026-01-13 12:42:33 +0530

    Fix API Base URL for Production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: '/api/',
     timeout: 60000, // 60 seconds
     withCredentials: true,
     headers: {
